c388c89600777ea97423a5226f79ddce6104e9d5
Alysson <antonioalvesdasilvaalysson0@Gmail.com>
2026-04-06 11:42:46 -0300

    little fix

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include "apps/calculator.h"
 #include "main.h"
 int main(int status){
-    if (status==0)
-    {
+    
+    if (status==0){
         k_print("-----Ola! bem vindo ao SampeOs,um OS feito em C e em CLI!-----");
         k_print("selecione oque voce quer fazer: 1. Calculadora 2.sair ");
         k_putchar("\n");
