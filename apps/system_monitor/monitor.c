9117f43440ce6a4e9f3ff6618555dedabdf6d996
Alysson <antonioalvesdasilvaalysson0@Gmail.com>
2026-04-12 17:56:07 -0300

    little fixes

--- apps/system_monitor/monitor.c
+++ apps/system_monitor/monitor.c
@@ -7,16 +7,16 @@ extern int check_funcionando;
 extern int check_interrupter;
 
 void monitor_setup() {
-    if (check_funcionando == 1) {
-        k_print("O sistema de verificação geral do kernel está funcionando corretamente.");
+    if (check_funcionando == 0) {
+        k_print("O sistema de verificação geral do kernel esta funcionando corretamente.");
     } else {
-        k_panic_message("ATENÇÃO: A VERIFICAÇÃO GERAL DE ERROS DESTE KERNEL ESTÁ DESATIVADA POR UM MOTIVO DESCONHECIDO. É ALTAMENTE RECOMENDADO REINICIAR O COMPUTADOR, POIS, A QUALQUER MOMENTO, ELE PODE REINICIAR OU TRAVAR SEM JUSTIFICATIVA.");
+        k_print("ATENÇAO: A VERIFICAÇÃO GERAL DE ERROS DESTE KERNEL ESTA DESATIVADA POR UM MOTIVO DESCONHECIDO. E ALTAMENTE RECOMENDADO REINICIAR O COMPUTADOR, POIS, A QUALQUER MOMENTO, ELE PODE REINICIAR OU TRAVAR SEM JUSTIFICATIVA.");
         return;
     }
 
-    if (check_interrupter == 1) {
-        k_print("A função do time interrupter handler está funcionando corretamente.");
+    if (check_interrupter == 0) {
+        k_print("A funcao do time interrupter handler está funcionando corretamente.");
     } else {
-        k_panic_message("ATENÇÃO: A FUNÇÃO TIME INTERRUPTER HANDLER NÃO ESTÁ FUNCIONANDO POR UM MOTIVO DESCONHECIDO. A VERIFICAÇÃO CONTRA STACK_OVERFLOW ESTÁ DESATIVADA. É RECOMENDADA REINICIAR O COMPUTADOR.");
+        k_print("ATENÇAO: A FUNÇÃO TIME INTERRUPTER HANDLER NÃO ESTÁ FUNCIONANDO POR UM MOTIVO DESCONHECIDO. A VERIFICAÇÃO CONTRA STACK_OVERFLOW ESTÁ DESATIVADA. É RECOMENDADA REINICIAR O COMPUTADOR.");
     }
 }
